acee3525ee9e53c0bd4e73406bda3c70092102a2
xiaoheng <xiaohenghzu@gmail.com>
2015-04-19 18:00:38 +0800

    代码对齐

--- zhaopin/spiders/zhaopin_pider.py
+++ zhaopin/spiders/zhaopin_pider.py
@@ -31,10 +31,10 @@ class CitySpider(Spider):
         sites_error = sel.xpath('//div[@class="no-resulte-warning"]').extract()
         if not sites_error:
             current_url = response._get_url()
-            pattern = ".*/pd1/p(.*)"
-            match_num = re.search(pattern, current_url, re.M | re.I)
-            num = string.atoi(match_num.group(1)) + 1
-            next_url = "http://jobs.zhaopin.com/pd1/p" + str(num)
+            pattern     = ".*/pd1/p(.*)"
+            match_num   = re.search(pattern, current_url, re.M | re.I)
+            num         = string.atoi(match_num.group(1)) + 1
+            next_url    = "http://jobs.zhaopin.com/pd1/p" + str(num)
             yield Request(next_url, callback=self.parse)    #return the request of the next page's
             
             sites_url = sel.xpath('//span[@class="search_list_zw search_list_first"]/a')
@@ -50,40 +50,40 @@ class CitySpider(Spider):
                 #     print "it is chongfu : %s" % url[0]
 
     def my_parse(self, response):
-        sel = Selector(response)
-        item_job = JobItem()
+        sel         = Selector(response)
+        item_job    = JobItem()
 
-        item_job["url"]        = response._get_url()[:-8]
-        #item_job["name"]       = sel.xpath('//div[@class="inner-left fl"]/h1/text()').extract()
-        item_job["name"]       = [n.encode('utf-8') for n in sel.xpath('//div[@class="inner-left fl"]/h1/text()').extract()]
-        item_job["company"]    = [n.encode('utf-8') for n in sel.xpath('//div[@class="inner-left fl"]/h2/a/text()').extract()]
-        item_job["com_url"]    = [n.encode('utf-8') for n in sel.xpath('//div[@class="inner-left fl"]/h2/a/@href').extract()]
-        item_job["welfare"]    = [n.encode('utf-8') for n in sel.xpath('//div[@class="inner-left fl"]/div/span/text()').extract()]
+        item_job["url"]         = response._get_url()[:-8]
+        # item_job["name"]        = sel.xpath('//div[@class="inner-left fl"]/h1/text()').extract()
+        item_job["name"]        = [n.encode('utf-8') for n in sel.xpath('//div[@class="inner-left fl"]/h1/text()').extract()]
+        item_job["company"]     = [n.encode('utf-8') for n in sel.xpath('//div[@class="inner-left fl"]/h2/a/text()').extract()]
+        item_job["com_url"]     = [n.encode('utf-8') for n in sel.xpath('//div[@class="inner-left fl"]/h2/a/@href').extract()]
+        item_job["welfare"]     = [n.encode('utf-8') for n in sel.xpath('//div[@class="inner-left fl"]/div/span/text()').extract()]
 
         sites = sel.xpath('//ul[@class="terminal-ul clearfix"]/li')
-        item_job["mon_pay"]    = [n.encode('utf-8') for n in sites[0].xpath('./strong/text()').extract()]
-        item_job["place"]      = [n.encode('utf-8') for n in sites[1].xpath('./strong/a/text()').extract()]
-        item_job["sub_place"]  = [n.encode('utf-8') for n in sites[1].xpath('./strong/text()').extract()]
-        item_job["date"]       = [n.encode('utf-8') for n in sites[2].xpath('./strong/span/text()').extract()]
-        item_job["job_prop"]   = [n.encode('utf-8') for n in sites[3].xpath('./strong/text()').extract()]
-        item_job["exper"]      = [n.encode('utf-8') for n in sites[4].xpath('./strong/text()').extract()]
-        item_job["edu"]        = [n.encode('utf-8') for n in sites[5].xpath('./strong/text()').extract()]
-        item_job["num"]        = [n.encode('utf-8') for n in sites[6].xpath('./strong/text()').extract()]
-        item_job["classify"]   = [n.encode('utf-8') for n in sites[7].xpath('./strong/a/text()').extract()]
+        item_job["mon_pay"]     = [n.encode('utf-8') for n in sites[0].xpath('./strong/text()').extract()]
+        item_job["place"]       = [n.encode('utf-8') for n in sites[1].xpath('./strong/a/text()').extract()]
+        item_job["sub_place"]   = [n.encode('utf-8') for n in sites[1].xpath('./strong/text()').extract()]
+        item_job["date"]        = [n.encode('utf-8') for n in sites[2].xpath('./strong/span/text()').extract()]
+        item_job["job_prop"]    = [n.encode('utf-8') for n in sites[3].xpath('./strong/text()').extract()]
+        item_job["exper"]       = [n.encode('utf-8') for n in sites[4].xpath('./strong/text()').extract()]
+        item_job["edu"]         = [n.encode('utf-8') for n in sites[5].xpath('./strong/text()').extract()]
+        item_job["num"]         = [n.encode('utf-8') for n in sites[6].xpath('./strong/text()').extract()]
+        item_job["classify"]    = [n.encode('utf-8') for n in sites[7].xpath('./strong/a/text()').extract()]
 
         sites = sel.xpath('//div[@class="tab-inner-cont"]')
         item_job["descr"]       = [n.encode('utf-8') for n in sites[0].xpath('.//*/text()').extract()]
-        item_job["com_intro"]  = [n.encode('utf-8') for n in sites[1].xpath('.//*/text()').extract()[2:]]
+        item_job["com_intro"]   = [n.encode('utf-8') for n in sites[1].xpath('.//*/text()').extract()[2:]]
 
         sites = sel.xpath('//div[@class="company-box"]')
         #item_job["com_url"]    = [n.encode('utf-8') for n in sites.xpath('./p/a/@href').extract()]
         #item_job["com_name"]   = [n.encode('utf-8') for n in sites.xpath('./p/a/text()').extract()]
 
         sites = sites.xpath('./ul/li')
-        item_job["com_scale"]  = [n.encode('utf-8') for n in sites[0].xpath('./strong/text()').extract()]
-        item_job["com_prop"]   = [n.encode('utf-8') for n in sites[1].xpath('./strong/text()').extract()]
-        item_job["com_indust"] = [n.encode('utf-8') for n in sites[2].xpath('./strong/a/text()').extract()]
-        item_job["com_dress"]  = [n.encode('utf-8') for n in sites[-1].xpath('./strong/text()').extract()]
+        item_job["com_scale"]   = [n.encode('utf-8') for n in sites[0].xpath('./strong/text()').extract()]
+        item_job["com_prop"]    = [n.encode('utf-8') for n in sites[1].xpath('./strong/text()').extract()]
+        item_job["com_indust"]  = [n.encode('utf-8') for n in sites[2].xpath('./strong/a/text()').extract()]
+        item_job["com_dress"]   = [n.encode('utf-8') for n in sites[-1].xpath('./strong/text()').extract()]
 
         if sites[3] != sites[-1]:
             item_job["com_home"]  = [n.encode('utf-8') for n in sites[3].xpath('./strong/a/text()').extract()]
